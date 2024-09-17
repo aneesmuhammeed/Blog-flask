988cacd25ebeb9ec9871d30851eaa70312d00568
anees <aneesk002@gmail.com>
2024-09-17 13:51:10 +0530

    lets goo

--- website/auth.py
+++ website/auth.py
@@ -6,7 +6,6 @@ import bcrypt
 
 auth = Blueprint("auth", __name__)
 
-
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -15,7 +14,7 @@ def login():
 
         user = User.query.filter_by(email=email).first()
         if user:
-            if bcrypt.checkpw(user.password, password.encode('utf-8')):
+            if bcrypt.checkpw(password, user.password):
                 flash("Logged in!", category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
@@ -26,14 +25,13 @@ def login():
 
     return render_template("login.html", user=current_user)
 
-
 @auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get("email")
         username = request.form.get("username")
-        password1 = request.form.get("password1").encode('utf-8')
-        password2 = request.form.get("password2").encode('utf-8')
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
 
         email_exists = User.query.filter_by(email=email).first()
         username_exists = User.query.filter_by(username=username).first()
@@ -43,7 +41,7 @@ def sign_up():
         elif username_exists:
             flash('Username is already in use.', category='error')
         elif password1 != password2:
-            flash('Password don\'t match!', category='error')
+            flash('Passwords don\'t match!', category='error')
         elif len(username) < 2:
             flash('Username is too short.', category='error')
         elif len(password1) < 6:
@@ -51,7 +49,8 @@ def sign_up():
         elif len(email) < 4:
             flash("Email is invalid.", category='error')
         else:
-            new_user = User(email=email, username=username, password=bcrypt.hashpw(password1, bcrypt.gensalt()))
+            hashed_password = bcrypt.hashpw(password1.encode('utf-8'), bcrypt.gensalt())
+            new_user = User(email=email, username=username, password=hashed_password)
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
@@ -59,7 +58,6 @@ def sign_up():
             return redirect(url_for('views.home'))
 
     return render_template("signup.html", user=current_user)
-
 
 @auth.route("/logout")
 @login_required
